0401db9b7cacf34f9ea498e669763e4891789b62
Ahonui <116345502+AhonuiHawaii@users.noreply.github.com>
2026-04-27 08:54:51 -1000

    Sutting up Vuetify

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -8,7 +8,11 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@assets': resolve('src/renderer/src/assets'),
+        '@store': resolve('src/renderer/src/stores'),
+        '@views': resolve('src/renderer/src/views'),
+        '@components': resolve('src/renderer/src/components')
       }
     },
     plugins: [vue()]
