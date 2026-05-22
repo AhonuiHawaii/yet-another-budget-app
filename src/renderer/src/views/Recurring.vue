e53cc2fd6acdc7849cdb20c6c9844b72f4196c12
Ahonui <116345502+AhonuiHawaii@users.noreply.github.com>
2026-05-22 12:25:38 -1000

    feat: add rescan button and improve component key handling in Recurring view

--- src/renderer/src/views/Recurring.vue
+++ src/renderer/src/views/Recurring.vue
@@ -10,11 +10,12 @@
       >
         <v-btn value="upcoming">Upcoming</v-btn>
         <v-btn value="all">All Recurring</v-btn>
+        <v-btn prepend-icon="mdi-refresh" :loading="scanning" @click.stop="rescan">Rescan</v-btn>
         <v-btn value="calendar">Calendar</v-btn>
       </v-btn-toggle>
     </div>
 
-    <component :is="currentComponent" />
+    <component :is="currentComponent" :key="componentKey" />
   </div>
 </template>
 
@@ -25,8 +26,16 @@ import AllRecurring from '../components/AllRecurring.vue'
 import Calendar from '../components/Calendar.vue'
 
 const activeTab = ref('upcoming')
+const scanning = ref(false)
+const componentKey = ref(0)
 
 const tabs = { upcoming: Upcoming, all: AllRecurring, calendar: Calendar }
-
 const currentComponent = computed(() => tabs[activeTab.value])
+
+async function rescan() {
+  scanning.value = true
+  await window.electron.ipcRenderer.invoke('transactions:rescanRecurring')
+  scanning.value = false
+  componentKey.value++
+}
 </script>
